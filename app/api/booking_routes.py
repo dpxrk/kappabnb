a09b896ffc40937448489a84b450535c3c49929c
Daniel <danielpark0503@gmail.com>
2021-03-29 18:44:37 -0400

    backEndRoute for booking

--- app/api/booking_routes.py
+++ app/api/booking_routes.py
@@ -1,2 +1,21 @@
 from flask import Blueprint, jsonify
-from app.models import db, Booking
+from app.models import db, Booking, Picture, Review, State, Comment, Reservation, User
+
+
+booking_route = Blueprint('booking', __name__)
+
+@booking_routes.routes('/')
+  def allBookings():
+    bookings = Booking.query.all()
+    return {"bookings" : [booking.to_dict() for booking in bookings]}
+
+
+@booking_routes.routes('/<int:id>')
+  def getOneBooking(id):
+    booking = Booking.query.get(id)
+    pictures = Picture.query.filter_by(bookingId=id).all()
+    reviews = Review.query.filter_by(bookingId=id).all()
+    host = User.query.get(booking.userId)
+    State = State.query.filter_by(bookingId=id).first()
+
+    return {"bookingData": [booking.to_dict() for booking in bookings]}
--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,17 +1,17 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import User
+from app.models import User, Booking, Reservation, Review
 
 user_routes = Blueprint('users', __name__)
 
-
+# Get all users
 @user_routes.route('/')
 @login_required
 def users():
     users = User.query.all()
     return {"users": [user.to_dict() for user in users]}
 
-
+#get specific user
 @user_routes.route('/<int:id>')
 @login_required
 def user(id):
