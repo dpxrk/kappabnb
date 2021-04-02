22b67b7c7044dabd6b87ba19e18d3e0c2bf65cdf
Daniel <danielpark0503@gmail.com>
2021-04-02 13:11:33 -0400

    moreFrontEnd

--- react-app/src/components/exploreSingleBooking/index.js
+++ react-app/src/components/exploreSingleBooking/index.js
@@ -4,14 +4,25 @@ import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 import "./exploreSingleBooking.css";
 import { useParams, useHistory } from "react-router-dom";
 import { getSingleBooking, getAllBookings } from "../../store/booking";
+import { DateRange } from "react-date-range";
+import { reserveBooking } from "../../store/reservation";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
 
 const ExploreSingleBooking = () => {
   const history = useHistory();
   const { id } = useParams();
   const [booking, setBooking] = useState({});
   const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state?.session?.user);
   const bookings = useSelector((state) => state.booking.listOfBookings);
-  // const [bookingIsClickedOn, setBookingIsClickedOn] = useState("false");
+  const [reservationDates, setReservationDates] = useState([
+    {
+      start: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
 
   const mapStyles = {
     height: "100%",
@@ -34,10 +45,30 @@ const ExploreSingleBooking = () => {
   };
 
   const handleMarkerClick = (e, id) => {
+    // e.preventDefault();
     // history.push(`/explore/${id}`);
   };
 
-  console.log("this is the current booking", booking);
+  const handleReservationSubmit = (e) => {
+    e.preventDefault();
+    if (
+      reservationDates[0].starteDate === null ||
+      reservationDates[0].endDate === null
+    ) {
+      alert("Please enter valid dates for your trip.");
+    } else {
+      dispatch(
+        reserveBooking(
+          sessionUser.id,
+          booking.id,
+          reservationDates[0].startDate,
+          reservationDates[0].endDate
+        )
+      );
+    }
+  };
+  const today = new Date();
+  console.log("this is the current booking", booking.pictures);
   return (
     <div className="explorerContainer">
       <div className="map">
@@ -64,34 +95,58 @@ const ExploreSingleBooking = () => {
       </div>
       <div className="rightHalfOfPage">
         <div className="centerContainer">
-          <div>
+          <h2>
             {" "}
             Welcome to <span className="bookingTitle">
               {booking.title}{" "}
-            </span> in <span className="bookingState">{booking.state} </span>
-          </div>
+            </span> in{" "}
+            <span className="bookingState">
+              {booking.state} hosted by: {booking.userId}{" "}
+            </span>
+          </h2>
           <div> </div>
-          <div> Here are some beautiful photos for you to check out! </div>
+          <h3> Here are some beautiful photos for you to check out! </h3>
           <div>
             {" "}
             {booking?.pictures?.forEach((picture) => (
-              <img src={picture} alt="">
-                {" "}
-                PHOTOS
-              </img>
+              <div key={picture.id}>
+                <img src={picture} alt="" />
+              </div>
             ))}
           </div>
-          <div>
+          <h3>
             The address to this location is: {booking.address}, {booking.state}{" "}
             {booking.capital}
-          </div>
+          </h3>
+          <h4>{booking.description}</h4>
           <div>
             Check out some reviews which have an average of:
             {booking?.reviews?.forEach((review) => (
               <div>{review} </div>
             ))}
           </div>
-          <div>The host is: {booking.userId}</div>
+          <div className="bookingForm">
+            <form onSubmit={(e) => handleReservationSubmit(e)}>
+              <div className="bookingFormTitle">
+                <h2>${booking.price}/night </h2>
+              </div>
+
+              <div className="bookingFormCalendar">
+                <DateRange
+                  minDate={today}
+                  editableDateInputs={true}
+                  onChange={(dates) => setReservationDates([dates.selection])}
+                  moveRangeOnFirstSelection={false}
+                  reservationDates={reservationDates}
+                />
+              </div>
+              <div>
+                <button type="submit" className="bookingFormButton">
+                  Book the capitol building
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
