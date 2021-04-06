3125f0398ae53af376c30d00c50ffaa2e82e12a7
Daniel <danielpark0503@gmail.com>
2021-04-06 15:54:27 -0400

    formatting review page

--- react-app/src/components/reviewReservation/index.js
+++ react-app/src/components/reviewReservation/index.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "./reviewReservation.css";
 
 const ReviewReservation = () => {
-  return <div> You made it to the reviews page</div>;
+  return (
+    <div className="entireReviewsContainer">
+      <div className="reviewContainer">
+        Check out other reviews and write your own
+        <hr className="lineUnderTitle" />
+        <div className="otherReviewsContainer"> You are here </div>
+      </div>
+    </div>
+  );
 };
 
 export default ReviewReservation;
